afc110bf673da2ced74aba383556518872adeed2
Cisco Ramon <aungkoman@gmail.com>
2025-01-02 08:23:45 +0630

    initial commit

---
+++ src/main/java/mm/com/software100/springhello/hello/HelloApplication.java
@@ -0,0 +1,13 @@
+package mm.com.software100.springhello.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mm/com/software100/springhello/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package mm.com.software100.springhello.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
