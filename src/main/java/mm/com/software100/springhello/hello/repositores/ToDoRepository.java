847bf24150b2719155c24aa71005f72b950d462a
Cisco Ramon <aungkoman@gmail.com>
2025-01-03 09:18:02 +0630

    repository added

--- src/main/java/mm/com/software100/springhello/hello/repositores/ToDoRepository.java
+++
@@ -1,9 +0,0 @@
-package mm.com.software100.springhello.hello.repositores;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import mm.com.software100.springhello.hello.entities.ToDo;
-
-public interface ToDoRepository extends JpaRepository<ToDo, Long> {
-    // You can define custom queries here if needed
-}
---
+++ src/main/java/mm/com/software100/springhello/hello/repository/ToDoRepository.java
@@ -0,0 +1,14 @@
+package mm.com.software100.springhello.hello.repositores;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import mm.com.software100.springhello.hello.entities.ToDo;
+
+public interface ToDoRepository extends CrudRepository<ToDo, Long> {
+    // You can define custom queries here if needed
+    List<ToDo> findByName(String name);
+
+    ToDo findById(long id);
+}
