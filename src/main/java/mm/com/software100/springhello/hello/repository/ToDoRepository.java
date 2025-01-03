01f0cd368f029560ce932cc9ac0ec7ae26fe726c
Cisco Ramon <aungkoman@gmail.com>
2025-01-03 09:26:36 +0630

    Add comments to clarify the use of Spring Data JPA in ToDoRepository

--- src/main/java/mm/com/software100/springhello/hello/repository/ToDoRepository.java
+++ src/main/java/mm/com/software100/springhello/hello/repository/ToDoRepository.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 import mm.com.software100.springhello.hello.entities.ToDo;
-
+/* we don't need to implement this interface, Spring  */
+/*
+ * that is what makes Spring Data JPA so powerful: You need not write an implementation of the repository interface. 
+ * Spring Data JPA creates an implementation when you run the application.
+ */
 public interface ToDoRepository extends CrudRepository<ToDo, Long> {
     // You can define custom queries here if needed
     List<ToDo> findByName(String name);
