a013d40a0cddbb747e7fc912c75a5d48b94d5b6e
Cisco Ramon <aungkoman@gmail.com>
2025-01-03 08:28:55 +0630

    Task entity added

--- src/main/java/mm/com/software100/springhello/hello/ToDoController.java
+++ src/main/java/mm/com/software100/springhello/hello/ToDoController.java
@@ -1,8 +1,14 @@
 package mm.com.software100.springhello.hello;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import mm.com.software100.springhello.hello.entities.Task;
 
 @RestController
 public class ToDoController {
-    
+    private final List<Task> tasks = new ArrayList<>();
 }
