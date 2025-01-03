022e44e86246abe1351e99c3280735480baf9d4d
Cisco Ramon <aungkoman@gmail.com>
2025-01-03 12:40:55 +0630

    user repository updated

--- src/main/java/mm/com/software100/springhello/hello/repository/UserRepository.java
+++ src/main/java/mm/com/software100/springhello/hello/repository/UserRepository.java
@@ -1,5 +1,12 @@
 package mm.com.software100.springhello.hello.repository;
 
-public class UserRepository {
-    
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import mm.com.software100.springhello.hello.entities.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    List<User> findByEmail(String email);
+
+    User findById(long id);
 }
