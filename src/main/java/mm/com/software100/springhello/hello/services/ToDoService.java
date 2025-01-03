962f22fecf56e2d9a18a4bcfb3c0ec3804180237
Cisco Ramon <aungkoman@gmail.com>
2025-01-03 09:24:49 +0630

    note added

--- src/main/java/mm/com/software100/springhello/hello/services/ToDoService.java
+++ src/main/java/mm/com/software100/springhello/hello/services/ToDoService.java
@@ -10,8 +10,20 @@ import mm.com.software100.springhello.hello.entities.ToDo;
 import mm.com.software100.springhello.hello.repository.ToDoRepository;
 
 public class ToDoService {
+    /*
+     * @Autowired is a Spring Framework annotation used for dependency injection. 
+     * It allows Spring to automatically inject the required dependencies into a class. 
+     * When you annotate a field, constructor, or setter method with @Autowired, 
+     * Spring's dependency injection mechanism will automatically provide the required bean.
+     */
     @Autowired
     private ToDoRepository toDoRepository;
+
+    /*
+     * In the context of Spring Framework, a bean is an object that is managed by the Spring IoC (Inversion of Control) container. 
+     * Beans are the fundamental building blocks of a Spring application. 
+     * They are created, configured, and managed by the Spring container.
+     */
 
     public List<ToDo> getAllToDos() {
         Iterable<ToDo> iterable = toDoRepository.findAll();
